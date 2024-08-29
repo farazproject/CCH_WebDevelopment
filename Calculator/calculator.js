0bcab872f002ef30c4ed017d432d2e04e7958267
farazproject <gulvirgo123@gmail.com>
2024-08-30 01:21:14 +0500

    porject completed

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,1 +1,26 @@
-console.log("ok");
+let display = document.querySelector(".display");
+let buttons = document.querySelectorAll("button");
+
+let output = "";
+
+console.log(output);
+
+// add event listeners to buttons and call calculate function on click.
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    //console.log(e.target.innerHTML);
+    let input = e.target.innerHTML;
+
+    if (input == "C") {
+      output = "";
+      display.value = output;
+    } else if (input == "=") {
+      output = eval(output);
+      display.value = output;
+    } else {
+      output += e.target.innerHTML;
+      display.value = output;
+    }
+  });
+});
